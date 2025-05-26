9187729bc9984dae57c239210e9cafe75a203f2a
IvanTangZhu19 <iefaz.ivantang@gmail.com>
2025-05-26 12:12:56 -0500

    feat: Creación de vistas

---
+++ app/src/main/java/co/edu/upb/numerosflash/views/Multiplayer.kt
@@ -0,0 +1,10 @@
+package co.edu.upb.numerosflash.views
+
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.navigation.NavController
+
+@Composable
+fun Multijugador(navController: NavController){
+    Text("Multijugador")
+}
