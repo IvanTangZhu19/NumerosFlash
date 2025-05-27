715e07ce8ff718fededc8c24280c5984c000769b
IvanTangZhu19 <iefaz.ivantang@gmail.com>
2025-05-27 13:31:22 -0500

    feat: Header falta color y logo

--- app/src/main/java/co/edu/upb/numerosflash/views/Home.kt
+++ app/src/main/java/co/edu/upb/numerosflash/views/Home.kt
@@ -1,10 +1,30 @@
 package co.edu.upb.numerosflash.views
 
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
+import co.edu.upb.numerosflash.layouts.Header
 
 @Composable
 fun Home(navController: NavController){
-    Text("Inicio")
+    Column(
+        modifier = Modifier.fillMaxWidth().padding(15.dp),
+        horizontalAlignment = Alignment.CenterHorizontally
+    ){
+        Header(navController)
+        Spacer(modifier = Modifier.height(20.dp))
+        Text(
+            "Inicio",
+            style = MaterialTheme.typography.headlineMedium
+        )
+    }
 }
