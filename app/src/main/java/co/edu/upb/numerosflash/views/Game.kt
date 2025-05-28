4f27d384dcfea5148e8b0a8235413970c2a43394
IvanTangZhu19 <iefaz.ivantang@gmail.com>
2025-05-28 12:00:23 -0500

    feat: Vista preliminar del juego

--- app/src/main/java/co/edu/upb/numerosflash/views/Game.kt
+++ app/src/main/java/co/edu/upb/numerosflash/views/Game.kt
@@ -1,10 +1,61 @@
 package co.edu.upb.numerosflash.views
 
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material3.DrawerValue
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.ModalNavigationDrawer
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
+import androidx.compose.ui.text.input.TextFieldValue
+import androidx.compose.material3.rememberDrawerState
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.input.KeyboardType
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.navigation.NavController
+import co.edu.upb.numerosflash.layouts.Header
+import co.edu.upb.numerosflash.layouts.sideMenu
 
 @Composable
 fun Game(navController: NavController){
-    Text("Juego")
+    val drawerState = rememberDrawerState(DrawerValue.Closed)
+    val scope = rememberCoroutineScope()
+    var num by remember{ mutableStateOf(TextFieldValue("")) }
+
+    ModalNavigationDrawer(
+        drawerState = drawerState,
+        drawerContent = {
+            sideMenu(navController)
+        }
+    ) {
+        Column(
+            modifier = Modifier.fillMaxWidth().padding(15.dp),
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            Header(navController, scope, drawerState)
+            Spacer(modifier = Modifier.height(20.dp))
+            Box(Modifier.fillMaxWidth().height(200.dp), contentAlignment = Alignment.Center){
+                Text(text="Numero", fontSize = 70.sp)
+            }
+            Text("Escribe la respuesta: ", style = MaterialTheme.typography.bodyLarge)
+            TextField(
+                value = num,
+                onValueChange = { num = it },
+                keyboardOptions = KeyboardOptions.Default.copy(keyboardType = KeyboardType.Number)
+            )
+        }
+    }
 }
