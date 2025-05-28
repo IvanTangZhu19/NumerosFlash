eb576854a20237f03d5f6a575831499c319a6c20
IvanTangZhu19 <iefaz.ivantang@gmail.com>
2025-05-28 12:18:14 -0500

    feat: viewmodel del juego

---
+++ app/src/main/java/co/edu/upb/numerosflash/viewmodels/Game.kt
@@ -0,0 +1,15 @@
+package co.edu.upb.numerosflash.viewmodels
+
+import androidx.lifecycle.ViewModel
+import co.edu.upb.numerosflash.models.Level
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+
+class Game: ViewModel(){
+    private val _nivel = MutableStateFlow<Level?>(null)
+    val nivel: StateFlow<Level?> = _nivel
+
+    fun seleccionarNivel(nivel: Level){
+        _nivel.value = nivel
+    }
+}
