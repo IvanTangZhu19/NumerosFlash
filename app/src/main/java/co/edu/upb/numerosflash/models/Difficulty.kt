af1ec7bb535701f296d5b02030876d2be1630980
IvanTangZhu19 <iefaz.ivantang@gmail.com>
2025-05-28 12:12:38 -0500

    feat: Modelos Dificultad y Niveles

---
+++ app/src/main/java/co/edu/upb/numerosflash/models/Difficulty.kt
@@ -0,0 +1,7 @@
+package co.edu.upb.numerosflash.models
+
+data class Difficulty(
+    var titulo: String,
+    var descripcion: String,
+    var levels: List<Level>
+)
---
+++ app/src/main/java/co/edu/upb/numerosflash/models/Level.kt
@@ -0,0 +1,9 @@
+package co.edu.upb.numerosflash.models
+
+data class Level(
+    var id: Int,
+    var rangoMin: Int,
+    var rangoMax: Int,
+    var tiempo: Int,
+    var numOperaciones: Int,
+)
