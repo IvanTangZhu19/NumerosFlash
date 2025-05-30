f1d0dc51f23bd9da93628624d6a51efafb997da4
IvanTangZhu19 <iefaz.ivantang@gmail.com>
2025-05-30 09:19:15 -0500

    feat: sonidos de aplausos y de incorrecto configurados

---
+++ app/src/main/java/co/edu/upb/numerosflash/sounds/sounds.kt
@@ -0,0 +1,32 @@
+package co.edu.upb.numerosflash.sounds
+
+import android.content.Context
+import android.media.AudioAttributes
+import android.media.SoundPool
+import co.edu.upb.numerosflash.R
+
+private lateinit var soundPool: SoundPool
+private var soundAplausos: Int = 0
+private var soundIncorrecto: Int = 0
+
+fun initSoundPool(context: Context){
+    val audioAttributes = AudioAttributes.Builder()
+        .setUsage(AudioAttributes.USAGE_GAME)
+        .setContentType(AudioAttributes.CONTENT_TYPE_SONIFICATION)
+        .build()
+
+    soundPool = SoundPool.Builder()
+        .setMaxStreams(5)
+        .setAudioAttributes(audioAttributes)
+        .build()
+    soundAplausos = soundPool.load(context, R.raw.applause, 1)
+    soundIncorrecto = soundPool.load(context, R.raw.fail, 1)
+}
+
+fun reproducirAplausos(){
+    soundPool.play(soundAplausos, 1f, 1f, 0, 0, 1f)
+}
+
+fun reproducirIncorrecto(){
+    soundPool.play(soundIncorrecto, 1f, 1f, 0, 0, 1f)
+}
