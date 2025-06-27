4b36217f24f7ba92cc2bd603cde9e732b4f00346
IvanTangZhu19 <iefaz.ivantang@gmail.com>
2025-06-27 17:39:55 -0500

    fix: Animación de pulsación para los numeros y navegacion del logo a creditos

--- app/src/main/java/co/edu/upb/numerosflash/views/Game.kt
+++ app/src/main/java/co/edu/upb/numerosflash/views/Game.kt
@@ -4,6 +4,11 @@ import android.util.Log
 import androidx.compose.animation.AnimatedContent
 import androidx.compose.animation.AnimatedVisibility
 import androidx.compose.animation.ExperimentalAnimationApi
+import androidx.compose.animation.core.FastOutSlowInEasing
+import androidx.compose.animation.core.RepeatMode
+import androidx.compose.animation.core.animateFloat
+import androidx.compose.animation.core.infiniteRepeatable
+import androidx.compose.animation.core.rememberInfiniteTransition
 import androidx.compose.animation.core.tween
 import androidx.compose.animation.fadeIn
 import androidx.compose.animation.fadeOut
@@ -36,6 +41,7 @@ import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.scale
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.Color.Companion.White
 import androidx.compose.ui.graphics.CompositingStrategy
@@ -204,6 +210,16 @@ fun Game(navController: NavController, gameViewModel: GameViewModel, userViewMod
 @OptIn(ExperimentalAnimationApi::class)
 @Composable
 fun AnimatedNumber(numero: Int?, visible: Boolean) {
+    //Animación de pulsación
+    val infiniteTransition = rememberInfiniteTransition()
+    val pulseScale by infiniteTransition.animateFloat(
+        initialValue = 0.9f,
+        targetValue = 1.2f,
+        animationSpec = infiniteRepeatable(
+            animation = tween(1000, easing = FastOutSlowInEasing),
+            repeatMode = RepeatMode.Reverse
+        )
+    )
     Box(
         modifier = Modifier
             .fillMaxWidth()
@@ -229,8 +245,10 @@ fun AnimatedNumber(numero: Int?, visible: Boolean) {
                         fontSize = 140.sp,
                         fontFamily = KanitFontFamily,
                         color = Amarrillo,
-                        modifier = Modifier.graphicsLayer {
-                            compositingStrategy = CompositingStrategy.Offscreen
+                        modifier = Modifier
+                                .scale(pulseScale)
+                                .graphicsLayer {
+                                compositingStrategy = CompositingStrategy.Offscreen
                         }
                     )
                 }
